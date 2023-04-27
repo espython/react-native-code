c2721355b9bb49c27388bf577e570211a61ed252
es_python <espython85@gmail.com>
2023-04-27 22:05:25 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@
  * React Native Flib App
  */
 import React from 'react';
-// import {useColorScheme} from 'react-native';
-// import {Colors} from 'react-native/Libraries/NewAppScreen';
 import { Provider } from 'react-redux';
 import { NativeBaseProvider } from 'native-base';
 import store from './redux/store';
